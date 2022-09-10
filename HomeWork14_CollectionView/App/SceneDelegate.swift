17c7712668b0e50505892f2f972f494a3846bb50
makeevs74 <makeevs74@gmail.com>
2022-09-10 23:11:56 +0500

    make project clean

---
+++ HomeWork14_CollectionView/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  HomeWork14_CollectionView
+//
+//  Created by Sergey Makeev on 10.09.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
