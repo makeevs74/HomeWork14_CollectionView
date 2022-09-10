aaa055a708f521e9f6a17e795913646818cab000
makeevs74 <makeevs74@gmail.com>
2022-09-10 23:00:36 +0500

    Initial Commit

---
+++ HomeWork14_CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork14_CollectionView
+//
+//  Created by Sergey Makeev on 10.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
